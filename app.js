33618b89f8cd86588c5fc549aab33c0035e53248
leestephens12 <lrsteph1@lakeheadu.ca>
2024-01-11 00:38:17 -0500

    added routes

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+var loginRouter = require('./routes/login');
+var registerRouter = require('./routes/register');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,7 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use(['/','/login'], loginRouter);
+app.use('/register', registerRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- utilities/Authentication.js
+++ utilities/Authentication.js
@@ -1,7 +1,7 @@
 //imports to conifgure app on client side
 const {initializeApp} = require('firebase/app');
 //imports to initialize the authencitcation 
-const {getAuth} = require('firebase/auth');
+const {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword} = require('firebase/auth');
 //importing necessary prebuilt files
 const firebase_config = require('./firebase-settings/firebase-configuration.js');
 
@@ -10,5 +10,33 @@ class Authentication {
     static app = initializeApp(firebase_config);
     //intialize the authentication
     static auth = getAuth(this.app);
+
+    //Register users in firebase aith with email and password
+    static async register(email, password) {
+        try {
+            await createUserWithEmailAndPassword(this.auth, email, password)
+                .then((userCredential) => {
+                    user = userCredential.user;
+                    console.log("user registered in successfully");
+                });
+        }catch(error) {
+            console.log(error);
+        }
+    }
+
+    //functio for user to log in 
+    static async signIn(email, password) {
+        //use built in sign in method from firebase passing trhough an email and password from front end
+        await signInWithEmailAndPassword(this.auth, email, password)
+        try {
+            await signInWithEmailAndPassword(this.auth, email, password)
+                .then((userCredential) => {
+                    user = userCredential.user;
+                    console.log("user signed in successfully");
+                });
+        }catch(error) {
+            console.log(error);
+        }
+    }
 
 } module.exports = Authentication;
