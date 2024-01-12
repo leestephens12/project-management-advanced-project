5fb947a1d83cb91c79a7d08e2161931473d87325
leestephens12 <lrsteph1@lakeheadu.ca>
2024-01-12 13:40:52 -0500

    Changed back to firebase

--- utilities/Firestore.js
+++ utilities/Firestore.js
@@ -1,4 +1,16 @@
-class Firestore {
+//firebase configuration imports
+ const {initializeApp, applicationDefault, cert} = require('firebase-admin/app');
+ //firestore configuration imports
+ const {getFirestore} = require('firebase-admin/firestore');
 
+ const service_account = require('./firebase-settings/firebase-service-accounts.json');
 
-} module.exports = Firestore;
+ class Firestore {
+
+     static app = initializeApp({
+         credential : cert(service_account)
+     });
+
+     static db = getFirestore(this.app);
+
+ } module.exports = Firestore; 
