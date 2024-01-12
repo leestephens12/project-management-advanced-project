37d69bf3fe98b9750a3c60758227042fda20c56a
leestephens12 <lrsteph1@lakeheadu.ca>
2024-01-12 16:56:38 -0500

    Logout function created

--- utilities/Authentication.js
+++ utilities/Authentication.js
@@ -1,7 +1,7 @@
 //imports to conifgure app on client side
 const {initializeApp} = require('firebase/app');
 //imports to initialize the authencitcation 
-const {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword} = require('firebase/auth');
+const {getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut} = require('firebase/auth');
 //importing necessary prebuilt files
 const firebase_config = require('./firebase-settings/firebase-configuration.js');
 
@@ -11,21 +11,47 @@ class Authentication {
     //intialize the authentication
     static auth = getAuth(this.app);
 
+    /**
+     * uses buil in sign in function through firebase to authenticate user
+     * In route file it redirects to correct page based on auth status
+     * @param {String} email -> User inputted password from Login Form
+     * @param {String} password -> User inputed password from front end
+     */
     static async login(email, password) {
         await signInWithEmailAndPassword(this.auth, email, password)
             .then((user) => {
                 console.log(user.user.email + ' logged in successfully');
+                
             }).catch((error) => {
                 console.log("There was an error logging in " + error);
             });
     }
 
+    /**
+     * uses buil in sign in function through firebase to register user
+     * Creates new account in firebase
+     * @param {String} email -> User inputted password from Login Form
+     * @param {String} password -> User inputed password from front end
+     */
     static async register(email, password) {
         await createUserWithEmailAndPassword(this.auth, email, password)
             .then((user) => {
-                console.log(user.user.email + ' registered successfully');
+                console.log(user.user.email + 'registered successfully');
             }).catch((error) => {
                 console.log("There was an error registering the user: " + error);
+            });
+    }
+
+    /**
+     * This is an async function users use to sign out
+     */
+    static async logout() {
+        await signOut(this.auth)
+            .then(()=> {
+                console.log("User signed out successfully");
+            })
+            .catch((error) => {
+                console.log("There was an error signing out: " + error);
             });
     }
 } module.exports = Authentication;
