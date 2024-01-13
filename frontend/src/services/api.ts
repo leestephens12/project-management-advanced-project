4663590af5f3d3fbebef9de885ec7a5e18a97fd2
samuelbluefin <samuel@bluefinenterprises.com>
2024-01-13 15:15:28 -0500

    added login api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,19 @@
+import axios from 'axios';
 
-export function loginUser(email: string, password: string): void {
-    console.log(email, '\n', password);
+const baseUrl = "http://localhost:5000";
+
+export function loginUser(email: string, password: string) {
+    const url = `${baseUrl}/login`;
+    const data = {
+        email: email,
+        password: password
+    };
+
+    axios.post(url, data)
+        .then(response => {
+            console.log('Login successful:', response.data);
+        })
+        .catch(error => {
+            console.error('Login error:', error);
+        });
 }
