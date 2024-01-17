5447fb09c49057780ac9a5893d803e9e93445f14
samuelbluefin <samuel@bluefinenterprises.com>
2024-01-17 13:28:55 -0500

    disabled form inputs while loading and corrected spinner colour

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -7,7 +7,7 @@ export const Login = ()  => {
     const navigate = useNavigate();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [error, setError] = useState({ title: null, message: null });
 
     const handleLogin = async (e: any): Promise<void> => {
@@ -52,6 +52,7 @@ export const Login = ()  => {
                                             name="email"
                                             type="email"
                                             onChange={(e) => setEmail(e.target.value)}
+                                            disabled={loading}
                                             autoComplete="email"
                                             required
                                             className="block w-full rounded-md border-0 px-1.5 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -70,6 +71,7 @@ export const Login = ()  => {
                                             name="password"
                                             type="password"
                                             onChange={(e) => setPassword(e.target.value)}
+                                            disabled={loading}
                                             autoComplete="current-password"
                                             required
                                             className="block w-full rounded-md border-0 px-1.5 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
@@ -83,6 +85,7 @@ export const Login = ()  => {
                                             id="remember-me"
                                             name="remember-me"
                                             type="checkbox"
+                                            disabled={loading}
                                             className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
                                         />
                                         <label htmlFor="remember-me"
