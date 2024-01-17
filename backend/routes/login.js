1a4546abb724aa3068d6d1e275d0c00d4bd5e0c0
leestephens12 <lrsteph1@lakeheadu.ca>
2024-01-17 12:42:56 -0500

    removed tokens for now

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -13,18 +13,18 @@ router.post('/', async function(req, res) {
   try {
     const { email, password } = req.body;
     await Authentication.login(email, password);
-    try {
+    /*try {
       const uid = await Authentication.getUserID();
       try {
         const token = await Firestore.createToken(uid);
         res.send({token});
       }catch(error) {
-        res.status(500).json({message: "Error creating a token", error: error.message});
+        res.json({message: "Error creating a token", error: error.message});
       }
     }
      catch(error) {
-      res.status(500).json({message: "Could not retrieve User ID", error: error.message});
-     }
+      res.json({message: "Could not retrieve User ID", error: error.message});
+     }*/
     console.log('user logged in successfully');
     res.status(200).json({ message: 'Login successful' });
   } catch(error) {
