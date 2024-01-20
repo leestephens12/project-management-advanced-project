5d22dbd192e27e8847c83086b72d1b5fb92d17c2
samuelbluefin <samuel@bluefinenterprises.com>
2024-01-20 12:05:00 -0500

    implemented get dashboard api

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,10 +1,15 @@
 import {NavBar} from "../components/NavBar";
 import {TaskPage} from "../components/TaskPage";
+import {getDashboard} from "../services/api";
 
 
-export const Dashboard = () => (
-    <div>
-        <NavBar />
-        <TaskPage />
-    </div>
-)
+export const Dashboard = () => {
+    getDashboard().then(console.log);
+
+    return (
+        <div>
+            <NavBar/>
+            <TaskPage/>
+        </div>
+    )
+}
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,13 @@
 import axios from 'axios';
+import {User} from "../models/User";
+
 
 const baseUrl = "http://localhost:3000";
+
+export async function getDashboard() {
+    const url = `${baseUrl}/dashboard`;
+    return axios.get(url);
+}
 
 export async function loginUser(email: string, password: string) {
     const url = `${baseUrl}/login`;
@@ -12,11 +19,17 @@ export async function loginUser(email: string, password: string) {
     return axios.post(url, data);
 }
 
-export async function registerUser(email: string, password: string) {
+export async function registerUser(user: User) {
     const url = `${baseUrl}/register`;
     const data = {
-        email: email,
-        password: password
+        _admin: user.admin,
+        _firstName: user.firstName,
+        _lastName: user.lastName,
+        _occupation: user.occupation,
+        _company: user.company,
+        _email: user.email,
+        _password: user.password,
+
     };
 
     return axios.post(url, data);
