c048956c1510dc1b8cbab2a60da8a7e198f19e67
samuelbluefin <samuel@bluefinenterprises.com>
2024-01-21 16:55:47 -0500

    implemented task fetching logic in task page

--- frontend/src/components/TaskPage.tsx
+++ frontend/src/components/TaskPage.tsx
@@ -1,4 +1,26 @@
-export const TaskPage = () => (
-    <div>
-    </div>
-)
+import {useEffect, useState} from "react";
+import {getDashboard} from "../services/api";
+import {TaskCard} from "./TaskCard";
+import {Task} from "../models/Task";
+
+export const TaskPage = () => {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        getDashboard().then((response) => {
+            setTasks(response.data.tasks);
+        });
+    })
+
+    return (
+        <div>
+            {
+                tasks.map((task: Task, count) => (
+                    <li key={count} className="flex items-center justify-between gap-x-6 py-5">
+                        <TaskCard task={task} />
+                    </li>
+                ))
+            }
+        </div>
+    )
+}
