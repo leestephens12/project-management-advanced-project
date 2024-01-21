eb5e7bceb7ad051e6134024be0ffb7cfb18d56bb
leestephens12 <lrsteph1@lakeheadu.ca>
2024-01-21 15:36:21 -0500

    Update addTask.js

--- backend/routes/addTask.js
+++ backend/routes/addTask.js
@@ -1,7 +1,8 @@
-const express = require("express");
 const Task = require("../models/Task");
-const Firestore = require("../utilities/Firestore");
-const router = express.Router();
+const express = require('express');
+const router = express.Router(); // Use Router instead of express()
+const Authentication = require('../utilities/Authentication');
+const Firestore = require('../utilities/Firestore');
 
 router.use(express.json());
 
@@ -9,9 +10,9 @@ router.post('/', async function(req,res) {
     const {name, assignee, description, status, dueDate, completionDate} = req.body;
     const task = new Task(name, assignee, description, status, dueDate, completionDate);
     try {
-        await Firestore.addDoc(JSON.parse(JSON.stringify(task)));
+        await Firestore.addDoc("tasks", JSON.parse(JSON.stringify(task)));
         res.status(200).json({message: "Task Uploaded to Database"});
     }catch(error) {
         res.status(500).json({message: "Task upload failed", error: error.message});
     }
-});
+}); module.exports = router;
