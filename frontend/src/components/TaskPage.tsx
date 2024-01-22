59c57f781ba408dde2b4882cff8bcb195817daf8
samuelbluefin <samuel@bluefinenterprises.com>
2024-01-22 15:13:14 -0500

    fixed dashboard api being called multiple times

--- frontend/src/components/TaskPage.tsx
+++ frontend/src/components/TaskPage.tsx
@@ -10,7 +10,7 @@ export const TaskPage = () => {
         getDashboard().then((response) => {
             setTasks(response.data.tasks);
         });
-    });
+    }, []);
 
     return (
         <div>
