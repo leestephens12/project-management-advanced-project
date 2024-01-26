043f1164b6041bbc3ccca89277dd49514a927fbc
samuelbluefin <samuel@bluefinenterprises.com>
2024-01-26 10:46:47 -0500

    added error handling in task creation

--- frontend/src/components/TaskPage.tsx
+++ frontend/src/components/TaskPage.tsx
@@ -1,7 +1,8 @@
 import {useEffect, useState} from "react";
-import {addTask, getDashboard} from "../services/api";
+import {createTask, getDashboard} from "../services/api";
 import {TaskCard} from "./TaskCard";
 import {Task} from "../models/Task";
+import {AddTaskModal} from "./AddTaskModal";
 
 export const TaskPage = () => {
     const [tasks, setTasks] = useState<Task[]>([]);
@@ -14,29 +15,40 @@ export const TaskPage = () => {
 
     const handleAddTask = async (e: any) => {
         e.preventDefault();
-        await addTask({
-            assignee: 'lee@test.com',
-            description: "Finsih this page",
-            name: "Finish Daahboard",
-            status: "In Progress",
-            teamID: "T#123",
-            dueDate:"2024-02-02",
-            completionDate: "Null"
-        });
+
+        try {
+            const task = {
+                assignee: 'pppp@test.com',
+                description: "",
+                name: "Finish Daahboard",
+                status: "In Progress",
+                teamID: "T#123",
+                dueDate:"2024-02-02",
+                completionDate: "Null"
+            };
+
+            await createTask(task);
+            setTasks([...tasks, task]);
+        } catch (err: any) {
+            throw new Error(`Could not create task: ${err.response.data.message}`);
+        } finally {
+
+        }
     }
 
     return (
-        <div>
+        <div className="p-10">
+            <AddTaskModal isOpen={true} />
             <button
                 type="button"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-indigo-600 mb-5 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 onClick={e => handleAddTask(e)}
             >
                 Add Task
             </button>
             {
                 tasks.map((task: Task, count) => (
-                    <li key={count} className="flex items-center justify-between gap-x-6 py-5">
+                    <li key={count} className="flex items-center justify-between mb-5">
                         <TaskCard task={task} />
                     </li>
                 ))
