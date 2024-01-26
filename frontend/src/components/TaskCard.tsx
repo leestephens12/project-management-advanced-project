236758eb90df386a2db2506a65571bc788669d48
samuelbluefin <samuel@bluefinenterprises.com>
2024-01-26 10:47:09 -0500

    styled task card

--- frontend/src/components/TaskCard.tsx
+++ frontend/src/components/TaskCard.tsx
@@ -2,10 +2,9 @@ import {Task} from "../models/Task";
 
 type TaskCardProps = { task: Task; }
 
-export const TaskCard = ( { task }: TaskCardProps) => {
+export const TaskCard = ({ task }: TaskCardProps) => {
     return (
-        <div>
-            <p className='font-bold'>{task.name}</p>
+        <div className="border rounded-xl px-3.5 pb-32 pt-3.5">
             <p>{task.description}</p>
             <p>Assigned to {task.assignee}</p>
         </div>
