beaec618fdd4568e7ac10e4a22d7dddc1984ad44
samuelbluefin <samuel@bluefinenterprises.com>
2024-01-31 16:45:06 -0500

    updated dropdown menu component

--- frontend/src/components/DropdownMenu.tsx
+++ frontend/src/components/DropdownMenu.tsx
@@ -6,18 +6,22 @@ function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ');
 }
 
-type DropdownMenuProps = { title: string; options?: string[]; }
+type MenuOption = {
+    name: string;
+    id?: string;
+}
+
+type DropdownMenuProps = { title: string; options: MenuOption[]; }
 
 export const DropdownMenu = ({ title, options }: DropdownMenuProps) => {
     return (
-        <Menu as="div" className="relative inline-block text-left">
+        <Menu as="div" className="relative inline-block text-left mt-6 ">
             <div>
                 <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                     {title}
                     <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
                 </Menu.Button>
             </div>
-
             <Transition
                 as={Fragment}
                 enter="transition ease-out duration-100"
@@ -27,62 +31,23 @@ export const DropdownMenu = ({ title, options }: DropdownMenuProps) => {
                 leaveFrom="transform opacity-100 scale-100"
                 leaveTo="transform opacity-0 scale-95"
             >
-                <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <Menu.Items className="absolute right-0 z-auto mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                     <div className="py-1">
-                        <Menu.Item>
-                            {({ active }) => (
-                                <a
-                                    href="#"
-                                    className={classNames(
-                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                        'block px-4 py-2 text-sm'
-                                    )}
-                                >
-                                    Account settings
-                                </a>
-                            )}
-                        </Menu.Item>
-                        <Menu.Item>
-                            {({ active }) => (
-                                <a
-                                    href="#"
-                                    className={classNames(
-                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                        'block px-4 py-2 text-sm'
-                                    )}
-                                >
-                                    Support
-                                </a>
-                            )}
-                        </Menu.Item>
-                        <Menu.Item>
-                            {({ active }) => (
-                                <a
-                                    href="#"
-                                    className={classNames(
-                                        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                        'block px-4 py-2 text-sm'
-                                    )}
-                                >
-                                    License
-                                </a>
-                            )}
-                        </Menu.Item>
-                        <form method="POST" action="#">
-                            <Menu.Item>
+                        {options.map((option) => (
+                            <Menu.Item key={option.name}>
                                 {({ active }) => (
-                                    <button
-                                        type="submit"
+                                    <a
+                                        href={option.id}
                                         className={classNames(
                                             active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                            'block w-full px-4 py-2 text-left text-sm'
+                                            'block px-4 py-2 text-sm'
                                         )}
                                     >
-                                        Sign out
-                                    </button>
+                                        {option.name}
+                                    </a>
                                 )}
                             </Menu.Item>
-                        </form>
+                        ))}
                     </div>
                 </Menu.Items>
             </Transition>
