8ac7a7e57cdc4fb375710401d1ac32e2434ed994
leestephens12 <lrsteph1@lakeheadu.ca>
2024-04-04 18:00:10 -0400

    Update getProjects.js

--- backend/routes/getProjects.js
+++ backend/routes/getProjects.js
@@ -7,8 +7,7 @@ router.use(express.json());
 router.get("/", async function (req, res) {
   try {
     //call the query docs function and returns a list of tasks based off of the logged in user
-    //const teamID = req.teamID;
-    const teamId = "W83IdUWRnCfbgjkBE778";
+    const teamID = req.query.teamID;
     const projects = await Firestore.queryDocs(
       "projects",
       "teamId",
