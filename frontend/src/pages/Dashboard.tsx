7a1a322346efcfb6cd8b9ad2b170f888ee573a99
samuelbluefin <samuel@bluefinenterprises.com>
2024-04-06 15:28:16 -0400

    misc

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,13 +1,15 @@
 import {NavBar} from "../components/NavBar";
 import {ProjectsPage} from "../components/ProjectsPage";
 import {TeamPage} from "../components/TeamPage";
+import {OverviewPage} from "../components/OverviewPage";
 
 export const Dashboard = () => {
     return (
         <div>
             <NavBar/>
-            <ProjectsPage/>
-            <TeamPage />
+            {/*<ProjectsPage/>*/}
+            <OverviewPage />
+            {/*<TeamPage />*/}
         </div>
     )
 }
